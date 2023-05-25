1b00645456fde4c4480a6a4673ebfc24c46e76e7
altergodzin <123497643+altergodzin@users.noreply.github.com>
2023-05-25 16:02:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    img = loadImage("dog_cat.jpg");
+
+}
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+}
+function draw() {
+    image(img,0,0,640,420);
+    fill(255,0,0);
+    text("dog",45,75);
+    noFill();
+    stroke(255,0,0);
+    rect(30,60,450,350);
+}
